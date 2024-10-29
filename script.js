c3f63e20d147c097a1bded4864e4ca14e7521449
PieselPolski <147921267+PieselPolski@users.noreply.github.com>
2024-10-29 22:45:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+
+const images = document.querySelectorAll('.floating-image');
+
+images.forEach((img) => {
+    let posX = Math.random() * window.innerWidth;
+    let posY = Math.random() * window.innerHeight;
+    let speedX = (Math.random() - 0.5) * 4;
+    let speedY = (Math.random() - 0.5) * 4;
+
+    function animate() {
+        posX += speedX;
+        posY += speedY;
+
+        // Odbijanie od granic ekranu
+        if (posX <= 0 || posX >= window.innerWidth - img.width) speedX *= -1;
+        if (posY <= 0 || posY >= window.innerHeight - img.height) speedY *= -1;
+
+        img.style.transform = `translate(${posX}px, ${posY}px)`;
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
