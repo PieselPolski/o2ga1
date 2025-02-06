d480678075bc6ade2b38e05ebf1d61fc02a22138
PieselPolski <147921267+PieselPolski@users.noreply.github.com>
2025-02-06 12:55:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const images = document.querySelectorAll('.floating-image');
 images.forEach((img) => {
     let posX = Math.random() * (window.innerWidth - img.offsetWidth);
     let posY = Math.random() * (window.innerHeight - img.offsetHeight);
-    let speedX = (Math.random() - 2) * 10; // Adjusted speed variation
-    let speedY = (Math.random() - 2) * 10;
+    let speedX = (Math.random() - 2) *  6; // Adjusted speed variation
+    let speedY = (Math.random() - 2) *  6;
 
     function animate() {
         posX += speedX;
